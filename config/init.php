5fe28b39af7e9540c6db4db61ea3d15b1a6ac659
Luka Tarkhnishvili <lukatarkhnishvili@gmail.com>
2019-11-13 16:21:25 +0400

    init project

---
+++ config/init.php
@@ -0,0 +1,23 @@
+<?php
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("www", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/ibuild/core');
+define("LIBS", ROOT . '/vendor/ibuild/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+// http://localhost:81/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+// http://localhost:81/public/
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+// http://localhost:81
+$app_path = str_replace('/public/', '', $app_path);
+define("PATCH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+
+require_once ROOT . '/vendor/autoload.php';
+
---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin_email' => 'admin@gmail.com',
+    'built_name' => 'Real Palace',
+    'pagination' => 4,
+    'smtp_login' => '',
+    'smtp_password' => ''
+];
